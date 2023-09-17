ba06d7d77fb9c4582a65853460bd6fee3754f7e4
victro-nuzhniy <nuzhniyva@gmail.com>
2023-09-17 22:00:36 +0300

    added flask-migrate functionality and models imports to __init__

--- api/__init__.py
+++ api/__init__.py
@@ -3,6 +3,7 @@ import os
 
 from dotenv import load_dotenv
 from flask import Flask
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 load_dotenv()
@@ -11,5 +12,24 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
 db = SQLAlchemy(app)
+
+from .models import (
+    Agreement,
+    Counterparty,
+    Discount,
+    Invoice,
+    InvoiceProducts,
+    Order,
+    OrderProducts,
+    Product,
+    PurchaseInvoice,
+    PurchaseInvoiceProducts,
+    SaleInvoice,
+    TaxInvoice,
+    TaxInvoiceProducts,
+    User,
+)
+
+migrate = Migrate(app, db)
 
 from api import routes
