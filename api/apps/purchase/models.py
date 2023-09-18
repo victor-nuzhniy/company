cf8ef523967caab98e853fb551bbeae7b375b841
victro-nuzhniy <nuzhniyva@gmail.com>
2023-09-18 19:14:39 +0300

    dev created purchase apps

---
+++ api/apps/purchase/models.py
@@ -0,0 +1,44 @@
+"""Models for purchase apps."""
+from api import db
+
+
+class PurchaseInvoice(db.Model):
+    """PurchaseInvoice model for api app."""
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    agreement_id = db.Column(
+        db.Integer,
+        db.ForeignKey("agreement.id", ondelete="CASCADE", onupdate="CASCADE"),
+        nullable=False,
+    )
+    created_at = db.Column(db.DateTime, nullable=False)
+    purchase_invoice_products = db.relationship("PurchaseInvoiceProducts")
+
+    def __repr__(self) -> str:
+        """Represent model instance."""
+        return str(self.name)
+
+
+class PurchaseInvoiceProducts(db.Model):
+    """PurchaseInvoiceProducts model for api app."""
+
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(
+        db.Integer,
+        db.ForeignKey("product.id", ondelete="CASCADE", onupdate="CASCADE"),
+        nullable=False,
+    )
+    quantity = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    purchase_invoice_id = db.Column(
+        db.Integer,
+        db.ForeignKey("purchase_invoice.id", ondelete="CASCADE", onupdate="CASCADE"),
+        nullable=False,
+    )
+    products_left = db.Column(db.Integer, nullable=False)
+    tax_invoice_products = db.relationship("TaxInvoiceProducts")
+
+    def __repr__(self) -> str:
+        """Represent model instance."""
+        return f"Purchase invoice product with id {self.product_id}"
