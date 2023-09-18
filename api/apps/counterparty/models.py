6e6a2834b9203272e9d5106fd0fb5dc190cd4089
victro-nuzhniy <nuzhniyva@gmail.com>
2023-09-18 19:12:53 +0300

    dev created counterparty apps

---
+++ api/apps/counterparty/models.py
@@ -0,0 +1,56 @@
+"""Counterparty app models."""
+from api import db
+
+
+class Discount(db.Model):
+    """Discount model for api app."""
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True, nullable=False)
+    rate = db.Column(db.Integer, nullable=False)  # TODO add validator
+    counterparties = db.relationship("Counterparty")
+
+    def __repr__(self) -> str:
+        """Represent model instance."""
+        return str(self.name)
+
+
+class Counterparty(db.Model):
+    """Counterparty model for api app."""
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), unique=True, nullable=False)
+    postal_code = db.Column(db.String(10))
+    country = db.Column(db.String(100))
+    city = db.Column(db.String(100))
+    address = db.Column(db.String(255))
+    phone_number = db.Column(db.String(30))  # TODO add validator
+    discount_id = db.Column(
+        db.Integer,
+        db.ForeignKey("discount.id", ondelete="CASCADE", onupdate="CASCADE"),
+        nullable=True,
+    )
+    agreements = db.relationship("Agreement")
+    orders = db.relationship("Order")
+
+    def __repr__(self) -> str:
+        """Represent model instance."""
+        return str(self.name)
+
+
+class Agreement(db.Model):
+    """Agreement model for api app."""
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), unique=True, nullable=False)
+    counterparty_id = db.Column(
+        db.Integer,
+        db.ForeignKey("counterparty.id", ondelete="CASCADE", onupdate="CASCADE"),
+        nullable=False,
+    )
+    invoices = db.relationship("Invoice")
+    purchase_invoices = db.relationship("PurchaseInvoice")
+
+    def __repr__(self) -> str:
+        """Represent model instance."""
+        return str(self.name)
