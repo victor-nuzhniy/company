a46eeae045fd6ac1eaf1c895686e2f75ee4d8f97
victro-nuzhniy <nuzhniyva@gmail.com>
2023-09-18 19:13:15 +0300

    dev created invoice apps

---
+++ api/apps/invoice/models.py
@@ -0,0 +1,68 @@
+"""Models for invoice apps."""
+from api import db
+
+
+class Invoice(db.Model):
+    """Invoice model for api app."""
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    order_id = db.Column(
+        db.Integer,
+        db.ForeignKey("order.id", ondelete="CASCADE", onupdate="CASCADE"),
+        nullable=False,
+    )
+    created_at = db.Column(db.DateTime, nullable=False)
+    paid = db.Column(db.Boolean, default=False)
+    agreement_id = db.Column(
+        db.Integer,
+        db.ForeignKey("agreement.id", ondelete="CASCADE", onupdate="CASCADE"),
+        nullable=False,
+    )
+    invoice_products = db.relationship("InvoiceProducts")
+    sale_invoices = db.relationship("SaleInvoice")
+
+    def __repr__(self) -> str:
+        """Represent model instance."""
+        return str(self.name)
+
+
+class InvoiceProducts(db.Model):
+    """InvoiceProducts model for api app."""
+
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(
+        db.Integer,
+        db.ForeignKey("product.id", ondelete="CASCADE", onupdate="CASCADE"),
+        nullable=False,
+    )
+    quantity = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    invoice_id = db.Column(
+        db.Integer,
+        db.ForeignKey("invoice.id", ondelete="CASCADE", onupdate="CASCADE"),
+        nullable=False,
+    )
+    tax_invoice_products = db.relationship("TaxInvoiceProducts")
+
+    def __repr__(self) -> str:
+        """Represent model instance."""
+        return f"Invoice product with id {self.product_id}"
+
+
+class SaleInvoice(db.Model):
+    """SaleInvoice model for api app."""
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    invoice_id = db.Column(
+        db.Integer,
+        db.ForeignKey("invoice.id", ondelete="CASCADE", onupdate="CASCADE"),
+        nullable=False,
+    )
+    created_at = db.Column(db.DateTime, nullable=False)
+    done = db.Column(db.Boolean, default=False)
+
+    def __repr__(self) -> str:
+        """Represent model instance."""
+        return str(self.name)
