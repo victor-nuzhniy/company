ff7b84c35f0fb4e44edaa68b11772a81784320f7
victro-nuzhniy <nuzhniyva@gmail.com>
2023-09-19 21:45:47 +0300

    def refactor token_required decorator

--- api/services.py
+++ api/services.py
@@ -1,6 +1,9 @@
-"""Operation with db."""
+"""Service functionality for api."""
+from functools import wraps
 from typing import Dict, List, Optional, Sequence
 
+import jwt
+from flask import current_app, request
 from flask_restful import Resource, abort, marshal_with
 from flask_restful.reqparse import RequestParser
 from sqlalchemy import (
@@ -13,7 +16,7 @@ from sqlalchemy import (
     update,
 )
 
-from api import db
+from api import User, db
 
 
 class CRUDOperations:
@@ -132,6 +135,50 @@ class CRUDOperations:
 crud = CRUDOperations()
 
 
+def token_required(is_admin: bool = False):
+    """Check token presence decorator."""
+
+    def decorator_factory(f):
+        @wraps(f)
+        def decorated(*args, **kwargs):
+            """Perform token checking."""
+            token: Optional[str] = None
+            if "Authorization" in request.headers:
+                token = request.headers["Authorization"].split(" ")[1]
+            if not token:
+                return {
+                    "message": "Authentication Token is missing!",
+                    "data": None,
+                    "error": "Unauthorized",
+                }, 401
+            try:
+                data = jwt.decode(
+                    token, current_app.config["SECRET_KEY"], algorithms=["HS256"]
+                )
+                current_user = crud.read(User, {"id": data.get("user_id")})
+            except Exception as e:
+                return {
+                    "message": "Something went wrong",
+                    "data": None,
+                    "error": str(e),
+                }, 500
+            if current_user is None:
+                return {
+                    "message": "Invalid Authentication token!",
+                    "data": None,
+                    "error": "Unauthorized",
+                }, 401
+            if not current_user.is_active:
+                abort(403)
+            if is_admin and not current_user.is_admin:
+                abort(403)
+            return f(*args, current_user=current_user, **kwargs)
+
+        return decorated
+
+    return decorator_factory
+
+
 class ModelRoute(Resource):
     """Operations with single model instance."""
 
@@ -147,22 +194,23 @@ class ModelRoute(Resource):
         ModelRoute.put = marshal_with(self.model_fields)(ModelRoute.put)
         ModelRoute.patch = marshal_with(self.model_fields)(ModelRoute.patch)
 
-    def get(self, instance_id):
+    def get(self, instance_id, *args, **kwargs):
         """Get model instance by id."""
         return crud.read(self.model, {"id": instance_id})
 
-    def put(self, instance_id):
+    @token_required(is_admin=True)
+    def put(self, instance_id, *args, **kwargs):
         """Update instance by id."""
         args = self.put_parser.parse_args()
         return crud.update(self.model, args, {"id": instance_id})
 
-    def patch(self, instance_id):
+    def patch(self, instance_id, *args, **kwargs):
         """Update instance bu id, partially."""
         args = self.patch_parser.parse_args()
         args = {key: value for key, value in args.items() if value}
         return crud.update(self.model, args, {"id": instance_id})
 
-    def delete(self, instance_id):
+    def delete(self, instance_id, *args, **kwargs):
         """Delete instance by id."""
         crud.delete(self.model, {"id": instance_id})
         return {
@@ -183,11 +231,11 @@ class ModelsRoute(Resource):
         ModelsRoute.post = marshal_with(self.model_fields)(ModelsRoute.post)
         ModelsRoute.get = marshal_with(self.model_fields)(ModelsRoute.get)
 
-    def post(self):
+    def post(self, *args, **kwargs):
         """Create model instance."""
         args = self.post_parser.parse_args()
         return crud.create(self.model, args)
 
-    def get(self):
+    def get(self, *args, **kwargs):
         """Get model instance list."""
         return crud.read_many(self.model)
