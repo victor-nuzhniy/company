b8816a784cae48a5490f3e6a435e9d7c6b05b284
victro-nuzhniy <nuzhniyva@gmail.com>
2023-09-19 22:46:16 +0300

    def added user_admin_patch_parser

--- api/apps/user/parsers.py
+++ api/apps/user/parsers.py
@@ -1,5 +1,6 @@
 """Parsers for user apps."""
 from flask_restful import reqparse
+from flask_restful.inputs import boolean
 
 from api.apps.user.validators import email, password, username
 
@@ -15,3 +16,7 @@ user_put_parser.add_argument("email", type=email, required=True)
 user_patch_parser = reqparse.RequestParser()
 user_patch_parser.add_argument("username", type=username, required=False)
 user_patch_parser.add_argument("email", type=email, required=False)
+
+user_admin_patch_parser = reqparse.RequestParser()
+user_admin_patch_parser.add_argument("is_active", type=boolean, required=False)
+user_admin_patch_parser.add_argument("is_admin", type=boolean, required=False)
