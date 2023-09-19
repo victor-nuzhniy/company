9d6a4f34ea28e8322aa9a272cd509732d0e1743e
victro-nuzhniy <nuzhniyva@gmail.com>
2023-09-19 16:58:45 +0300

    def refactor user password validator

--- api/apps/auth/utils.py
+++ api/apps/auth/utils.py
@@ -1,7 +1,7 @@
 """Utilities for auth apps."""
 from typing import Optional
 
-from werkzeug.security import check_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 
 from api import User
 from api.services import crud
@@ -18,3 +18,8 @@ def login(email: str, password: str) -> Optional[User]:
 def disable_account(user_id: int) -> User:
     """Disable user account - is_active to False."""
     return crud.update(User, {"is_active": False}, {"id": user_id})
+
+
+def encrypt_password(password: str) -> str:
+    """Encrypt password."""
+    return generate_password_hash(password, salt_length=4)  # TODO change length
