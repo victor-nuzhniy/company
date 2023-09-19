586e3a6858d1c9b55a35fcee18053576d27638d3
victro-nuzhniy <nuzhniyva@gmail.com>
2023-09-19 16:38:29 +0300

    def added is_active to User model

--- api/apps/user/models.py
+++ api/apps/user/models.py
@@ -10,6 +10,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
+    is_active = db.Column(db.Boolean, default=False)
     orders = db.relationship("Order")
 
     def __repr__(self) -> str:
