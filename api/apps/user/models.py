8acdec2d1c05db3d12bf094e07552c0cca218ef0
victro-nuzhniy <nuzhniyva@gmail.com>
2023-09-19 16:57:50 +0300

    def enlarged password field in User model

--- api/apps/user/models.py
+++ api/apps/user/models.py
@@ -8,7 +8,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
+    password = db.Column(db.String(120), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
     is_active = db.Column(db.Boolean, default=False)
     orders = db.relationship("Order")
