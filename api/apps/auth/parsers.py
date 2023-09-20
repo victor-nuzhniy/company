46cc05b075be10ef3504b2bb583738a8914844a2
victro-nuzhniy <nuzhniyva@gmail.com>
2023-09-20 22:25:04 +0300

    dev added admin_password validator

--- api/apps/auth/parsers.py
+++ api/apps/auth/parsers.py
@@ -1,8 +1,12 @@
 """Parsers for auth apps."""
 from flask_restful import reqparse
 
-from api.apps.user.validators import email
+from api.apps.user.parsers import user_post_parser
+from api.apps.user.validators import admin_password, email
 
 login_parser = reqparse.RequestParser()
 login_parser.add_argument("email", type=email, required=True)
 login_parser.add_argument("password", required=True)
+
+admin_parser = user_post_parser.copy()
+admin_parser.add_argument("admin_password", type=admin_password, required=True)
