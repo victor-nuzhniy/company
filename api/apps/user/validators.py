67c9f4426688f4f4f181448aafa657bcc3946271
victro-nuzhniy <nuzhniyva@gmail.com>
2023-09-20 22:24:25 +0300

    dev refactored user validators with abort, added admin_password validator

--- api/apps/user/validators.py
+++ api/apps/user/validators.py
@@ -1,5 +1,8 @@
 """Validators for user apps."""
+import os
 import re
+
+from flask import abort
 
 from api.apps.auth.utils import encrypt_password
 
@@ -7,12 +10,13 @@ from api.apps.auth.utils import encrypt_password
 def username(username_str) -> str:
     """Validate username."""
     if len(username_str) > 50:
-        raise ValueError(f"{username_str} length shoud be lower.")
+        abort(422, f"{username_str} length shoud be lower.")
     if not re.search(r"^[\w.@+-]+\Z", username_str):
-        raise ValueError(
+        abort(
+            422,
             "Enter a valid username. This value may contain"
             " only English letters, "
-            "numbers, and @/./+/-/_ characters."
+            "numbers, and @/./+/-/_ characters.",
         )
     return username_str
 
@@ -20,7 +24,7 @@ def username(username_str) -> str:
 def email(email_str) -> str:
     """Validate email."""
     if len(email_str) > 120:
-        raise ValueError(f"{email_str} length should be lower.")
+        abort(422, f"{email_str} length should be lower.")
     specials = "!#$%&'*+-/=?^_`{|?."
     specials = re.escape(specials)
     regex = (
@@ -37,12 +41,24 @@ def email(email_str) -> str:
         + "])@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$"
     )
     if not re.fullmatch(regex, email_str):
-        raise ValueError(f"Invalid email address format: {email_str}")
+        abort(422, f"Invalid email address format: {email_str}")
     return email_str
 
 
 def password(password_str: str) -> str:
     """Validate and encrypt password."""
     if len(password_str) < 1:  # TODO bigger int
-        raise ValueError("Password length is too short.")
+        abort(422, "Password length is too short.")
     return encrypt_password(password_str)
+
+
+def admin_password(admin_password_str: str) -> str:
+    """Validate admin_header."""
+    key = os.getenv("ADMIN_PASSWORD")
+    if not key:
+        abort(403)
+    if not admin_password_str:
+        abort(422, "Access field is empty.")
+    if key == admin_password_str:
+        return admin_password_str
+    abort(403, "Access denied. Value is invalid.")
