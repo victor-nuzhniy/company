892fbf089ceef404d21a8c6f872e0c15f9d7d0eb
victro-nuzhniy <nuzhniyva@gmail.com>
2023-09-20 12:08:13 +0300

    def added discount_id and counterparty_id validators in counterparty apps

--- api/apps/counterparty/validators.py
+++ api/apps/counterparty/validators.py
@@ -1,4 +1,8 @@
 """Validators for counterparty apps."""
+from flask import abort
+
+from api import Counterparty, Discount
+from api.services import db_utils
 
 
 def discount_name(name_str):
@@ -6,3 +10,17 @@ def discount_name(name_str):
     if len(name_str) > 30:
         raise ValueError(f"{name_str} length should be lower than 30 character.")
     return name_str
+
+
+def discount_id(discount_id_int) -> int:
+    """Validate discount id."""
+    if db_utils.is_exists(Discount, {"id": discount_id_int}):
+        return discount_id_int
+    abort(409, f"Discount with id {discount_id_int} does not exist.")
+
+
+def counterparty_id(counterparty_id_int) -> int:
+    """Validate counterparty_id."""
+    if db_utils.is_exists(Counterparty, {"id": counterparty_id_int}):
+        return counterparty_id_int
+    abort(409, f"Counterparty with id {counterparty_id_int} does not exist.")
