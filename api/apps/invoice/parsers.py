54937d2369c1fd4dc8122a1f14c0d3b817357dae
victro-nuzhniy <nuzhniyva@gmail.com>
2023-09-20 11:12:11 +0300

    def modified invoice parsers

--- api/apps/invoice/parsers.py
+++ api/apps/invoice/parsers.py
@@ -2,16 +2,18 @@
 from flask_restful import reqparse
 from flask_restful.inputs import boolean, datetime_from_iso8601
 
+from api.apps.invoice.validators import agreement_id, order_id
+
 invoice_parser = reqparse.RequestParser()
 invoice_parser.add_argument("name", required=True)
-invoice_parser.add_argument("order_id", type=int, required=True)
-invoice_parser.add_argument("agreement_id", type=int, required=True)
+invoice_parser.add_argument("order_id", type=order_id, required=True)
+invoice_parser.add_argument("agreement_id", type=agreement_id, required=True)
 
 invoice_patch_parser = reqparse.RequestParser()
 invoice_parser.add_argument("name", required=False)
-invoice_patch_parser.add_argument("order_id", type=int, required=False)
+invoice_patch_parser.add_argument("order_id", type=order_id, required=False)
 invoice_patch_parser.add_argument(
     "created_at", type=datetime_from_iso8601, required=False
 )
 invoice_patch_parser.add_argument("paid", type=boolean, required=False)
-invoice_patch_parser.add_argument("agreement_id", type=int, required=False)
+invoice_patch_parser.add_argument("agreement_id", type=agreement_id, required=False)
