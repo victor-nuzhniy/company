8055f8de5ec69548632d3cc8d8f1c27cdfe3463e
victro-nuzhniy <nuzhniyva@gmail.com>
2023-09-22 10:08:24 +0300

    dev added parsers to tax apps routes

--- api/apps/tax/routes.py
+++ api/apps/tax/routes.py
@@ -2,6 +2,12 @@
 from flask_restful import fields
 
 from api import TaxInvoice, TaxInvoiceProducts, api
+from api.apps.tax.parsers import (
+    tax_invoice_parser,
+    tax_invoice_patch_parser,
+    tax_invoice_products_parser,
+    tax_invoice_products_patch_parser,
+)
 from api.model_routes import ModelRoute, ModelsRoute
 
 tax_invoice_fields = {
@@ -24,8 +30,8 @@ class TaxInvoiceRoute(ModelRoute):
     """Operations with single TaxInvoice instance."""
 
     model = TaxInvoice
-    put_parser = None
-    patch_parser = None
+    put_parser = tax_invoice_parser
+    patch_parser = tax_invoice_patch_parser
     model_fields = tax_invoice_fields
 
 
@@ -33,7 +39,7 @@ class TaxInvoicesRoute(ModelsRoute):
     """Operations with TaxInvoice instances and instance creation."""
 
     model = TaxInvoice
-    post_parser = None
+    post_parser = tax_invoice_parser
     model_fields = tax_invoice_fields
 
 
@@ -41,8 +47,8 @@ class TaxInvoiceProductsRoute(ModelRoute):
     """Operations with single TaxInvoiceProducts instance."""
 
     model = TaxInvoiceProducts
-    put_parser = None
-    patch_parser = None
+    put_parser = tax_invoice_products_parser
+    patch_parser = tax_invoice_products_patch_parser
     model_fields = tax_invoice_products_fields
 
 
@@ -50,7 +56,7 @@ class ManyTaxInvoiceProductsRoute(ModelsRoute):
     """Operations with TaxInvoiceProducts instances and instance creation."""
 
     model = TaxInvoiceProducts
-    post_parser = None
+    post_parser = tax_invoice_products_parser
     model_fields = tax_invoice_products_fields
 
 
