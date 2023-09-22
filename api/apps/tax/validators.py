54997ad46047a8be92d2d0900b0076b89b951f4d
victro-nuzhniy <nuzhniyva@gmail.com>
2023-09-22 16:03:50 +0300

    dev corrected tax validator imports

--- api/apps/tax/validators.py
+++ api/apps/tax/validators.py
@@ -1,7 +1,7 @@
 """Validators for tax apps."""
 from flask import abort
 
-from api import InvoiceProducts, PurchaseInvoiceProducts, TaxInvoice
+from api import InvoiceProduct, PurchaseInvoiceProduct, TaxInvoice
 from api.services import db_utils
 
 
@@ -14,7 +14,7 @@ def tax_invoice_id(tax_invoice_id_int: int) -> int:
 
 def invoice_products_id(invoice_products_id_int: int) -> int:
     """Validate order_id."""
-    if db_utils.is_exists(InvoiceProducts, {"id": invoice_products_id_int}):
+    if db_utils.is_exists(InvoiceProduct, {"id": invoice_products_id_int}):
         return invoice_products_id_int
     abort(409, f"Order with id {invoice_products_id_int} does not exist.")
 
@@ -22,7 +22,7 @@ def invoice_products_id(invoice_products_id_int: int) -> int:
 def purchase_invoice_products_id(purchase_invoice_products_id_int: int) -> int:
     """Validate order_id."""
     if db_utils.is_exists(
-        PurchaseInvoiceProducts, {"id": purchase_invoice_products_id_int}
+        PurchaseInvoiceProduct, {"id": purchase_invoice_products_id_int}
     ):
         return purchase_invoice_products_id_int
     abort(409, f"Order with id {purchase_invoice_products_id_int} does not exist.")
