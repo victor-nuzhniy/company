dc30a94a896f57239c8fd5ff5699dd91324b95fe
victro-nuzhniy <nuzhniyva@gmail.com>
2023-09-24 16:52:48 +0300

    dev added ProductsLeftoversRoute to account routes

--- api/apps/account/routes.py
+++ api/apps/account/routes.py
@@ -1,15 +1,22 @@
 """Routes for account apps."""
-from flask_restful import Resource, fields, marshal_with
+from flask_restful import Resource, fields, marshal, marshal_with
 
 from api import SaleInvoiceProduct, api
-from api.apps.account.parsers import account_parser, period_parser
+from api.apps.account.parsers import (
+    account_parser,
+    period_parser,
+    product_leftovers_parser,
+)
 from api.apps.account.services import (
     create_tax_invoice_products,
     get_purchase_products_by_invoice_products,
+    get_purchase_products_quantity_list,
     get_sale_invoice_products_by_period,
+    get_sold_products,
     prepare_tax_invoice_products,
     update_sale_invoice,
 )
+from api.apps.account.utils import get_product_leftovers_on_date
 from api.services import crud
 
 period_report_fields = {
@@ -22,6 +29,15 @@ period_report_fields = {
     "units": fields.String,
     "code": fields.String,
     "currency": fields.String,
+}
+
+
+product_leftovers_fields = {
+    "id": fields.Integer,
+    "name": fields.String,
+    "units": fields.String,
+    "currency": fields.String,
+    "quantity": fields.Integer,
 }
 
 
@@ -57,5 +73,19 @@ class PeriodReportRoute(Resource):
         return get_sale_invoice_products_by_period(args)
 
 
+class ProductsLeftoversRoute(Resource):
+    """Product leftovers to particular date."""
+
+    def post(self, *args, **kwargs):
+        """Create product leftovers report."""
+        args = product_leftovers_parser.parse_args()
+        purchase_products = marshal(
+            get_purchase_products_quantity_list(args), product_leftovers_fields
+        )
+        sold_products = get_sold_products(args)
+        return get_product_leftovers_on_date(purchase_products, sold_products)
+
+
 api.add_resource(ProcessSaleInvoiceRoute, "/process-sale-invoice/")
 api.add_resource(PeriodReportRoute, "/sale-report/")
+api.add_resource(ProductsLeftoversRoute, "/product-leftovers/")
