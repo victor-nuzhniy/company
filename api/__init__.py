7c5a6cb71ba125808ce6528bd2bc9bbef08c6427
victro-nuzhniy <nuzhniyva@gmail.com>
2023-09-25 16:32:56 +0300

    dev added swagger initialization

--- api/__init__.py
+++ api/__init__.py
@@ -5,14 +5,19 @@ from dotenv import load_dotenv
 from flask import Flask
 from flask_migrate import Migrate
 from flask_restful import Api
+from flask_restful_swagger import swagger
 from flask_sqlalchemy import SQLAlchemy
 
 load_dotenv()
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["DEFAULT_RENDERERS"] = [
+    "flask_api.renderers.JSONRenderer",
+    "flask_api.renderers.BrowsableAPIRenderer",
+]
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
-api = Api(app)
+api = swagger.docs(Api(app), apiVersion="0.1")
 db = SQLAlchemy(app)
 
 from api.apps.counterparty.models import Agreement, Counterparty, Discount
@@ -26,13 +31,13 @@ from api.apps.user.models import User
 
 migrate = Migrate(app, db)
 
-from api.apps.user import routes
-from api.apps.counterparty import routes
+from api.apps.account import routes
 from api.apps.auth import routes
+from api.apps.counterparty import routes
 from api.apps.invoice import routes
 from api.apps.order import routes
 from api.apps.product import routes
 from api.apps.purchase import routes
-from api.apps.tax import routes
-from api.apps.account import routes
 from api.apps.sale import routes
+from api.apps.tax import routes
+from api.apps.user import routes
