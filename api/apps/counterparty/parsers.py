090bc049b05f9b4c7b9c8eb649c4723b8c7ddd44
victro-nuzhniy <nuzhniyva@gmail.com>
2023-09-29 20:50:05 +0300

    new_tests corrected counterparty parsers and validators

--- api/apps/counterparty/parsers.py
+++ api/apps/counterparty/parsers.py
@@ -2,34 +2,43 @@
 from flask_restful import reqparse
 from flask_restful.inputs import int_range
 
-from api.apps.counterparty.validators import counterparty_id, discount_id, discount_name
+from api.apps.counterparty.validators import (
+    counterparty_id,
+    discount_id,
+    str_length_10,
+    str_length_30,
+    str_length_150,
+    str_length_255,
+)
+from api.apps.invoice.validators import str_length_100
+from api.apps.product.validators import str_length_200
 
 discount_parser = reqparse.RequestParser()
-discount_parser.add_argument("name", type=discount_name, required=True)
+discount_parser.add_argument("name", type=str_length_30, required=True)
 discount_parser.add_argument("rate", type=int_range(low=0, high=100), required=True)
 
 discount_patch_parser = reqparse.RequestParser()
-discount_patch_parser.add_argument("name", type=discount_name)
+discount_patch_parser.add_argument("name", type=str_length_30)
 discount_patch_parser.add_argument("rate", type=int_range(low=0, high=100))
 
 counterparty_parser = reqparse.RequestParser()
-counterparty_parser.add_argument("name", required=True)
-counterparty_parser.add_argument("postal_code")
-counterparty_parser.add_argument("country")
-counterparty_parser.add_argument("city")
-counterparty_parser.add_argument("address")
-counterparty_parser.add_argument("phone_number")
+counterparty_parser.add_argument("name", type=str_length_150, required=True)
+counterparty_parser.add_argument("postal_code", type=str_length_10)
+counterparty_parser.add_argument("country", type=str_length_100)
+counterparty_parser.add_argument("city", type=str_length_100)
+counterparty_parser.add_argument("address", type=str_length_255)
+counterparty_parser.add_argument("phone_number", type=str_length_30)
 counterparty_parser.add_argument("discount_id", type=discount_id, required=True)
 
 counterparty_patch_parser = counterparty_parser.copy()
-counterparty_patch_parser.replace_argument("name")
+counterparty_patch_parser.replace_argument("name", type=str_length_150)
 counterparty_patch_parser.replace_argument("discount_id", type=discount_id)
 
 
 agreement_parser = reqparse.RequestParser()
-agreement_parser.add_argument("name", required=True)
+agreement_parser.add_argument("name", type=str_length_200, required=True)
 agreement_parser.add_argument("counterparty_id", type=counterparty_id, required=True)
 
 agreement_patch_parser = reqparse.RequestParser()
-agreement_patch_parser.add_argument("name")
+agreement_patch_parser.add_argument("name", type=str_length_200)
 agreement_patch_parser.add_argument("counterparty_id", type=counterparty_id)
--- api/apps/counterparty/validators.py
+++ api/apps/counterparty/validators.py
@@ -5,7 +5,7 @@ from api import Counterparty, Discount
 from api.services import db_utils
 
 
-def discount_name(name_str):
+def str_length_30(name_str):
     """Validate discount_name."""
     if len(name_str) > 30:
         abort(422, f"{name_str} length should be lower than 30 character.")
@@ -37,4 +37,11 @@ def str_length_255(name_str: str) -> str:
     """Validate arg length."""
     if len(name_str) > 255:
         abort(422, f"{name_str} length should be lower then 255 character.")
+    return name_str
+
+
+def str_length_10(name_str: str) -> str:
+    """Validate arg length."""
+    if len(name_str) > 10:
+        abort(422, f"{name_str} length should be lower then 10 character.")
     return name_str
