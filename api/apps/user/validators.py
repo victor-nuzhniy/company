10707ae2bc426886a10dc0b29f6d8f6744d03c67
victro-nuzhniy <nuzhniyva@gmail.com>
2023-09-29 22:51:52 +0300

    new_tests refactor validators exceptions

--- api/apps/user/validators.py
+++ api/apps/user/validators.py
@@ -2,18 +2,15 @@
 import os
 import re
 
-from flask import abort
-
 from api.apps.auth.utils import encrypt_password
 
 
 def username(username_str) -> str:
     """Validate username."""
     if len(username_str) > 50:
-        abort(422, f"{username_str} length shoud be lower.")
+        raise ValueError(f"{username_str} length shoud be lower.")
     if not re.search(r"^[\w.@+-]+\Z", username_str):
-        abort(
-            422,
+        raise ValueError(
             "Enter a valid username. This value may contain"
             " only English letters, "
             "numbers, and @/./+/-/_ characters.",
@@ -24,7 +21,7 @@ def username(username_str) -> str:
 def email(email_str) -> str:
     """Validate email."""
     if len(email_str) > 120:
-        abort(422, f"{email_str} length should be lower.")
+        raise ValueError(f"{email_str} length should be lower.")
     specials = "!#$%&'*+-/=?^_`{|?."
     specials = re.escape(specials)
     regex = (
@@ -41,14 +38,14 @@ def email(email_str) -> str:
         + "])@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$"
     )
     if not re.fullmatch(regex, email_str):
-        abort(422, f"Invalid email address format: {email_str}")
+        raise ValueError(f"Invalid email address format: {email_str}")
     return email_str
 
 
 def password(password_str: str) -> str:
     """Validate and encrypt password."""
     if len(password_str) < 1:  # TODO bigger int
-        abort(422, "Password length is too short.")
+        raise ValueError("Password length is too short.")
     return encrypt_password(password_str)
 
 
@@ -56,9 +53,9 @@ def admin_password(admin_password_str: str) -> str:
     """Validate admin_header."""
     key = os.getenv("ADMIN_PASSWORD")
     if not key:
-        abort(403)
+        raise ValueError("Forbidden.")
     if not admin_password_str:
-        abort(422, "Access field is empty.")
+        raise ValueError("Access field is empty.")
     if key == admin_password_str:
         return admin_password_str
-    abort(403, "Access denied. Value is invalid.")
+    raise ValueError("Access denied. Value is invalid.")
