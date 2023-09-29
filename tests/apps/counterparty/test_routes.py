eb43774424627e682d774ada762b4af82ed8e940
victro-nuzhniy <nuzhniyva@gmail.com>
2023-09-29 19:29:46 +0300

    new_tests added tests to counterparty test_tables

--- tests/apps/counterparty/test_routes.py
+++ tests/apps/counterparty/test_routes.py
@@ -1,5 +1,9 @@
 """Module for testing counterparty routes."""
+import json
+from typing import Dict
+
 import pytest
+from faker import Faker
 from flask import url_for
 
 from api import Discount
@@ -8,13 +12,71 @@ from tests.apps.counterparty.factories import DiscountFactory
 
 @pytest.mark.usefixtures("client_class")
 class TestGetDiscountRoute:
-    """Class for testing discount routes."""
+    """Class for testing discount get single instance by id route."""
 
-    def test_get_route(self) -> None:
+    def test_get_route(self, auth_header: Dict) -> None:
         """Test get route."""
         discount: Discount = DiscountFactory()
-        response = self.client.get(url_for("discountsroute", instance_id=discount.id))
-        result = response.get_json()[0]
+        response = self.client.get(
+            url_for("discountroute", instance_id=discount.id),
+            headers=auth_header,
+        )
+        result = response.get_json()
         assert response.status_code == 200
         for key, value in result.items():
             assert getattr(discount, key) == value
+
+    def test_get_route_not_exist(self, faker: Faker, auth_header: Dict) -> None:
+        """Test get route with not existed instance."""
+        discount_id: int = faker.random_int(min=9000)
+        response = self.client.get(
+            url_for("discountroute", instance_id=discount_id),
+            headers=auth_header,
+        )
+        result = response.get_json()
+        assert response.status_code == 404
+        assert (
+            result.get("message")
+            == f"Discount with data ['id: {discount_id}'] doesn't exist"
+        )
+
+
+@pytest.mark.usefixtures("client_class")
+class TestPutDiscountRoute:
+    """Class for testing Discount put single instance route."""
+
+    def test_put_route(self, faker: Faker, auth_header: Dict) -> None:
+        """Test put route."""
+        discount: Discount = DiscountFactory()
+        expected_data: Dict = {
+            "name": faker.pystr(min_chars=4, max_chars=10),
+            "rate": faker.random_int(min=0, max=100),
+        }
+        response = self.client.put(
+            url_for("discountroute", instance_id=discount.id),
+            headers=auth_header,
+            data=json.dumps(expected_data),
+        )
+        result = response.get_json()
+        assert response.status_code == 200
+        for key, value in expected_data.items():
+            assert result[key] == value
+
+    def test_put_route_invalid_rate(self, faker: Faker, auth_header: Dict) -> None:
+        """Test put route. Involid rate."""
+        discount: Discount = DiscountFactory()
+        expected_data: Dict = {
+            "name": faker.pystr(min_chars=4, max_chars=10),
+            "rate": faker.random_int(min=101),
+        }
+        response = self.client.put(
+            url_for("discountroute", instance_id=discount.id),
+            headers=auth_header,
+            data=json.dumps(expected_data),
+        )
+        result = response.get_json()
+        assert response.status_code == 400
+        assert result.get("message") == {
+            "rate": f"Invalid argument: {expected_data.get('rate')}. "
+            f"argument must be within the range 0 - 100"
+        }
