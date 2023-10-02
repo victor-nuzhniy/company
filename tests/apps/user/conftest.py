0e7ec7c50203064bdfa965b993f5b0c9b6b08667
victro-nuzhniy <nuzhniyva@gmail.com>
2023-10-02 12:33:49 +0300

    new_tests modified create_user_put_data

--- tests/apps/user/conftest.py
+++ tests/apps/user/conftest.py
@@ -18,7 +18,4 @@ def create_user_put_data(faker: Faker) -> Dict:
     return {
         "username": faker.user_name(),
         "email": faker.email(),
-        "password": faker.pystr(min_chars=40, max_chars=104),
-        "is_admin": faker.pybool(),
-        "is_active": faker.pybool(),
     }
