546b5f2cb999dcb90abda494a332f8fbd2038365
victro-nuzhniy <nuzhniyva@gmail.com>
2023-10-02 22:19:08 +0300

    new_tests fixed "parser" issue in tax parsers

--- api/apps/tax/parsers.py
+++ api/apps/tax/parsers.py
@@ -2,7 +2,8 @@
 from flask_restful import reqparse
 from flask_restful.inputs import datetime_from_iso8601
 
-from api.apps.invoice.validators import invoice_id, str_length_100
+from api.apps.invoice.validators import str_length_100
+from api.apps.sale.validators import sale_invoice_id
 from api.apps.tax.validators import (
     purchase_invoice_products_id,
     sale_invoice_products_id,
@@ -11,11 +12,15 @@ from api.apps.tax.validators import (
 
 tax_invoice_post_parser = reqparse.RequestParser()
 tax_invoice_post_parser.add_argument("name", type=str_length_100, required=True)
-tax_invoice_post_parser.add_argument("sale_invoice_id", type=invoice_id, required=True)
+tax_invoice_post_parser.add_argument(
+    "sale_invoice_id", type=sale_invoice_id, required=True
+)
 
 tax_invoice_put_parser = reqparse.RequestParser()
 tax_invoice_put_parser.add_argument("name", type=str_length_100, required=True)
-tax_invoice_put_parser.add_argument("sale_invoice_id", type=invoice_id, required=True)
+tax_invoice_put_parser.add_argument(
+    "sale_invoice_id", type=sale_invoice_id, required=True
+)
 tax_invoice_put_parser.add_argument(
     "created_at", type=datetime_from_iso8601, required=True
 )
@@ -23,7 +28,7 @@ tax_invoice_put_parser.add_argument(
 tax_invoice_patch_parser = reqparse.RequestParser()
 tax_invoice_patch_parser.add_argument("name", type=str_length_100, required=False)
 tax_invoice_patch_parser.add_argument(
-    "sale_invoice_id", type=invoice_id, required=False
+    "sale_invoice_id", type=sale_invoice_id, required=False
 )
 tax_invoice_patch_parser.add_argument(
     "created_at", type=datetime_from_iso8601, required=False
