5ec701476d5d7e5889d2f8205789f283905e8cc7
victro-nuzhniy <nuzhniyva@gmail.com>
2023-10-02 17:43:34 +0300

    new_tests created purchase test_routes

---
+++ tests/apps/purchase/test_routes.py
@@ -0,0 +1,46 @@
+"""Routes for purchase apps."""
+from typing import Dict
+
+from faker import Faker
+
+from api import PurchaseInvoice, PurchaseInvoiceProduct
+from tests.apps.purchase.conftest import (
+    create_purchase_invoice_data,
+    create_purchase_invoice_product_data,
+    create_purchase_invoice_put_data,
+)
+from tests.apps.purchase.factories import (
+    PurchaseInvoiceFactory,
+    PurchaseInvoiceProductFactory,
+)
+from tests.testing_classes import SampleTestRoute
+
+
+class TestPurchaseInvoiceRoute(SampleTestRoute):
+    """Class with methods for testing PurchaseInvoice routes."""
+
+    model = PurchaseInvoice
+    factory = PurchaseInvoiceFactory
+
+    def get_fake_data(self, faker: Faker) -> Dict:
+        """Get PurchaseInvoice fake data dict."""
+        return create_purchase_invoice_data(faker)
+
+    def get_fake_put_data(self, faker: Faker) -> Dict:
+        """Get PurchaseInvoice fake data dict for put and patch methods."""
+        return create_purchase_invoice_put_data(faker)
+
+
+class TestPurchaseInvoiceProductRoute(SampleTestRoute):
+    """Class with methods for testing PurhcaseInvoiceProduct routes."""
+
+    model = PurchaseInvoiceProduct
+    factory = PurchaseInvoiceProductFactory
+
+    def get_fake_data(self, faker: Faker) -> Dict:
+        """Get PurchaseInvoiceProduct fake data dict."""
+        return create_purchase_invoice_product_data(faker)
+
+    def get_fake_put_data(self, faker: Faker) -> Dict:
+        """Get PurchaseInvoiceProduct fake data dict for put and patch methods."""
+        return create_purchase_invoice_product_data(faker)
