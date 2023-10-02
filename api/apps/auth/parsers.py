1f9a546aadb2ec475f7b2657cc8cd62dd266e2e8
victro-nuzhniy <nuzhniyva@gmail.com>
2023-10-02 20:24:56 +0300

    new_tests modified user and auth parsers

--- api/apps/auth/parsers.py
+++ api/apps/auth/parsers.py
@@ -1,12 +1,14 @@
 """Parsers for auth apps."""
 from flask_restful import reqparse
 
-from api.apps.user.parsers import user_post_parser
-from api.apps.user.validators import admin_password, email
+from api.apps.user.validators import admin_password, email, password, username
 
 login_parser = reqparse.RequestParser()
 login_parser.add_argument("email", type=email, required=True)
 login_parser.add_argument("password", required=True)
 
-admin_parser = user_post_parser.copy()
+admin_parser = reqparse.RequestParser()
+admin_parser.add_argument("username", type=username, required=True)
+admin_parser.add_argument("email", type=email, required=True)
+admin_parser.add_argument("password", type=password, required=True)
 admin_parser.add_argument("admin_password", type=admin_password, required=True)
--- api/apps/user/parsers.py
+++ api/apps/user/parsers.py
@@ -4,18 +4,20 @@ from flask_restful.inputs import boolean
 
 from api.apps.user.validators import email, password, username
 
-user_post_parser = reqparse.RequestParser()
-user_post_parser.add_argument("username", type=username, required=True)
-user_post_parser.add_argument("email", type=email, required=True)
-user_post_parser.add_argument("password", type=password, required=False)
+user_parser = reqparse.RequestParser()
+user_parser.add_argument("username", type=username, required=True)
+user_parser.add_argument("email", type=email, required=True)
+user_parser.add_argument("password", type=password, required=True)
+user_parser.add_argument("is_admin", type=boolean, required=False)
+user_parser.add_argument("is_active", type=boolean, required=False)
 
-user_put_parser = reqparse.RequestParser()
-user_put_parser.add_argument("username", type=username, required=True)
-user_put_parser.add_argument("email", type=email, required=True)
 
 user_patch_parser = reqparse.RequestParser()
 user_patch_parser.add_argument("username", type=username, required=False)
 user_patch_parser.add_argument("email", type=email, required=False)
+user_patch_parser.add_argument("password", type=password, required=False)
+user_patch_parser.add_argument("is_admin", type=boolean, required=False)
+user_patch_parser.add_argument("is_active", type=boolean, required=False)
 
 user_admin_patch_parser = reqparse.RequestParser()
 user_admin_patch_parser.add_argument("is_active", type=boolean, required=False)
