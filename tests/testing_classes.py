f24457080dbca465a2b8597e2298b16a99225c05
victro-nuzhniy <nuzhniyva@gmail.com>
2023-10-02 20:22:50 +0300

    new_tests refactor testing_classes test_delete_route and test_get_many_route

--- tests/testing_classes.py
+++ tests/testing_classes.py
@@ -7,7 +7,7 @@ import pytest
 from faker import Faker
 from flask import url_for
 
-from api import db
+from api import User, db
 from tests.bases import BaseModelFactory
 from tests.conftest import check_instance_expected_data, delete_random_dict_key
 
@@ -75,15 +75,16 @@ class SampleTestRoute:
         )
         check_instance_expected_data(response, expected_data)
 
-    def test_delete_route(self, auth_header: Dict) -> None:
+    def test_delete_route(self, auth_header: Dict, app) -> None:
         """Test delete route."""
         instance: db.Model = self.factory()
+        instance_id: int = instance.id
         response = self.client.delete(
             url_for(f"{self.model.__name__.lower()}route", instance_id=instance.id),
             headers=auth_header,
         )
         assert response.status_code == 200
-        assert not self.model.query.all()
+        assert not self.model.query.filter_by(id=instance_id).first()
 
     def test_post_route(self, faker: Faker, auth_header: Dict) -> None:
         """Test post route."""
@@ -96,9 +97,11 @@ class SampleTestRoute:
         )
         check_instance_expected_data(response, expected_data)
 
-    def test_get_many_route(self, auth_header: Dict) -> None:
+    def test_get_many_route(self, auth_header: Dict, admin_user: User) -> None:
         """Test get route. Model list expected."""
         instances: List[db.Model] = self.factory.create_batch(size=5)
+        if self.model == User:
+            instances = [admin_user] + instances
         response = self.client.get(
             url_for(f"{self.model.__name__.lower()}sroute"),
             headers=auth_header,
