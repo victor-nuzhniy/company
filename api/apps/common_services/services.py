91c27014d9b947663f87e7fc80ec7b3e9724d150
victro-nuzhniy <nuzhniyva@gmail.com>
2024-01-16 18:48:47 +0200

    formatters changed common_services services

---
+++ api/apps/common_services/services.py
@@ -0,0 +1,34 @@
+"""Services for common_services apps."""
+import inspect
+
+from flask import abort
+from sqlalchemy import Row
+
+from api import Invoice, Order, PurchaseInvoice, SaleInvoice, TaxInvoice
+
+
+def get_last_name(model_name: str) -> Row:
+    """Get last model name."""
+    model_names_set: set = {
+        "Order",
+        "Invoice",
+        "PurchaseInvoice",
+        "SaleInvoice",
+        "TaxInvoice",
+    }
+    if model_name not in model_names_set:
+        abort(
+            409,
+            "".join((
+                "'{name}' is not in ".format(name=model_name),
+                "Order, Invoice, PurchaseInvoice, SaleInvoice, TaxInvoice models.",
+            )),
+        )
+    caller_globals = inspect.stack()[1][0].f_globals
+    model: Order | Invoice | PurchaseInvoice | SaleInvoice | TaxInvoice | None = (
+        caller_globals.get(model_name)
+    )
+    if model:
+        query = model.query.with_entities(model.name)
+        return query.order_by(model.id.desc()).first()
+    abort(500, "{name} not in globals".format(name=model_name))
