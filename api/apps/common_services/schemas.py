76ad440c128872fc69451e2bb64856a21baadb77
victro-nuzhniy <nuzhniyva@gmail.com>
2024-01-16 18:41:46 +0200

    formatters changed common_services schemas

---
+++ api/apps/common_services/schemas.py
@@ -0,0 +1,29 @@
+"""Schemas for common_services apps."""
+
+
+name_number_schema = {
+    "notes": "Get number for document name based on last name number.",
+    "nickname": "Get number for document name based on last name number.",
+    "parameters": [
+        {
+            "name": "Authorization",
+            "description": "Authorization: Bearer token",
+            "required": True,
+            "allowMultiple": False,
+            "dataType": "String",
+            "paramType": "header",
+            "defaultValue": "Bearer ",
+        },
+    ],
+    "responseClass": "NameNumber",
+    "responseMessages": [
+        {"code": 200, "message": "Operation successfully performed"},
+        {"code": 400, "message": "Invalid input (specified)."},
+        {"code": 401, "message": "Unauthorized."},
+        {
+            "code": 409,
+            "message": "'model_name' is not in Order, Invoice,"
+            " PurchaseInvoice, SaleInvoice, TaxInvoice models.",
+        },
+    ],
+}
