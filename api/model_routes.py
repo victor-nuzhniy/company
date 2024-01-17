ac5ecdd27c0038f6fcc286ddc1729204f6d230b3
victro-nuzhniy <nuzhniyva@gmail.com>
2024-01-17 21:30:36 +0200

    formatters format api model_routes.py

--- api/model_routes.py
+++ api/model_routes.py
@@ -1,55 +1,40 @@
 """Model routes and its functionality."""
 from functools import wraps
-from typing import Dict, Optional
+from typing import Any, Callable, Dict, Optional
 
-import jwt
-from flask import abort, current_app, request
+from flask import request
+from flask.typing import ResponseReturnValue
 from flask_restful import Resource, marshal
 from flask_restful.reqparse import RequestParser
+from sqlalchemy import Row
 
-from api import User, db
+from api import ModelType
+from api.api_utilities import (
+    check_current_user,
+    check_unique,
+    get_current_user,
+    get_token,
+    raise_forbidden_error,
+)
+from api.constants import server_error
 from api.services import crud
-from api.utils import check_unique
 
 
-def token_required(is_admin: bool = False):
+def token_required(is_admin: bool = False) -> Callable:
     """Check token presence decorator."""
 
-    def decorator_factory(f):
-        @wraps(f)
-        def decorated(*args, **kwargs):
+    def decorator_factory(func: Callable) -> Callable:  # noqa WPS430
+        @wraps(func)
+        def decorated(*args: Any, **kwargs: Any) -> ResponseReturnValue:  # noqa WPS430
             """Perform token checking."""
-            token: Optional[str] = None
-            if "Authorization" in request.headers:
-                token = request.headers["Authorization"].split(" ")[1]
-            if not token:
-                return {
-                    "message": "Authentication Token is missing!",
-                    "data": None,
-                    "error": "Unauthorized",
-                }, 401
+            token = get_token(request)
+            current_user: Optional[Row] = None
             try:
-                data = jwt.decode(
-                    token, current_app.config["SECRET_KEY"], algorithms=["HS256"]
-                )
-                current_user = crud.read(User, {"id": data.get("user_id")})
-            except Exception as e:
-                return {
-                    "message": "Authentication token is invalid",
-                    "data": None,
-                    "error": str(e),
-                }, 400
-            if current_user is None:
-                return {
-                    "message": "Invalid Authentication token!",
-                    "data": None,
-                    "error": "Unauthorized",
-                }, 401
-            if not current_user.is_active:
-                abort(403, "Current user is not active.")
-            if is_admin and not current_user.is_admin:
-                abort(403, "Current user is not admin.")
-            return f(*args, current_user=current_user, **kwargs)
+                current_user = get_current_user(token)
+            except Exception as ex:
+                raise_forbidden_error(str(ex))
+            check_current_user(current_user, is_admin)
+            return func(*args, current_user=current_user, **kwargs)
 
         return decorated
 
@@ -59,53 +44,85 @@ def token_required(is_admin: bool = False):
 class ModelRoute(Resource):
     """Operations with single model instance."""
 
-    model: Optional[db.Model] = None
+    model: Optional[ModelType] = None
     put_parser: Optional[RequestParser] = None
     patch_parser: Optional[RequestParser] = None
     model_fields: Optional[Dict] = None
 
-    def get(self, instance_id, *args, **kwargs):
+    def get(self, instance_id: int, *args: Any, **kwargs: Any) -> ResponseReturnValue:
         """Get model instance by id."""
-        return marshal(crud.read(self.model, {"id": instance_id}), self.model_fields)
+        if self.model:
+            return marshal(
+                crud.read(self.model, {"id": instance_id}),
+                self.model_fields,
+            )
+        return server_error
 
-    def put(self, instance_id, *args, **kwargs):
+    def put(self, instance_id: int, *args: Any, **kwargs: Any) -> ResponseReturnValue:
         """Update instance by id."""
-        args = self.put_parser.parse_args()
-        check_unique(self.model, args, instance_id)
-        return marshal(
-            crud.update(self.model, args, {"id": instance_id}), self.model_fields
-        )
+        if self.put_parser and self.model:
+            arguments = self.put_parser.parse_args()
+            check_unique(self.model, arguments, instance_id)
+            return marshal(
+                crud.update(self.model, arguments, {"id": instance_id}),
+                self.model_fields,
+            )
+        return server_error
 
-    def patch(self, instance_id, *args, **kwargs):  # TODO add token_decorator to all
+    def patch(
+        self,
+        instance_id: int,
+        *args: Any,
+        **kwargs: Any,
+    ) -> ResponseReturnValue:  # TODO add token_decorator to all
         """Update instance by id, partially."""
-        args = self.patch_parser.parse_args()
-        args = {key: value for key, value in args.items() if value is not None}
-        check_unique(self.model, args, instance_id)
-        return marshal(
-            crud.update(self.model, args, {"id": instance_id}), self.model_fields
-        )
+        if self.patch_parser and self.model:
+            arguments = self.patch_parser.parse_args()
+            arguments = {
+                key: elem for key, elem in arguments.items() if elem is not None
+            }
+            check_unique(self.model, arguments, instance_id)
+            return marshal(
+                crud.update(self.model, arguments, {"id": instance_id}),
+                self.model_fields,
+            )
+        return server_error
 
-    def delete(self, instance_id, *args, **kwargs):
+    def delete(
+        self,
+        instance_id: int,
+        *args: Any,
+        **kwargs: Any,
+    ) -> ResponseReturnValue:
         """Delete instance by id."""
-        crud.delete(self.model, {"id": instance_id})
-        return {
-            "message": f"Deleted {self.model.__name__} instance with id {instance_id}"
-        }
+        if self.model:
+            crud.delete(self.model, {"id": instance_id})
+            return {
+                "message": "Deleted {name} instance with id {id}".format(
+                    name=self.model.__name__,
+                    id=instance_id,
+                ),
+            }
+        return server_error
 
 
 class ModelsRoute(Resource):
     """Operations with multiple model instance and instance creation."""
 
-    model: Optional[db.Model] = None
+    model: Optional[ModelType] = None
     post_parser: Optional[RequestParser] = None
     model_fields: Optional[Dict] = None
 
-    def post(self, *args, **kwargs):
+    def post(self, *args: Any, **kwargs: Any) -> ResponseReturnValue:
         """Create model instance."""
-        args = self.post_parser.parse_args()
-        check_unique(self.model, args)
-        return marshal(crud.create(self.model, args), self.model_fields)
+        if self.post_parser and self.model:
+            arguments = self.post_parser.parse_args()
+            check_unique(self.model, arguments)
+            return marshal(crud.create(self.model, arguments), self.model_fields)
+        return server_error
 
-    def get(self, *args, **kwargs):
+    def get(self, *args: Any, **kwargs: Any) -> ResponseReturnValue:
         """Get model instance list."""
-        return marshal(crud.read_many(self.model, rev=True), self.model_fields)
+        if self.model:
+            return marshal(crud.read_many(self.model, rev=True), self.model_fields)
+        return server_error
