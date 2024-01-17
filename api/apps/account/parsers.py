5ae29e77e403c32caafb2c00a22c0077e9714acc
victro-nuzhniy <nuzhniyva@gmail.com>
2024-01-17 18:59:55 +0200

    formatters format account parsers

--- api/apps/account/parsers.py
+++ api/apps/account/parsers.py
@@ -2,10 +2,12 @@
 from flask_restful import reqparse
 from flask_restful.inputs import date
 
-from api.apps.sale.validators import sale_invoice_id
+from api.apps.sale.validators import sale_invoice_id_valid
 
 account_parser = reqparse.RequestParser()
-account_parser.add_argument("sale_invoice_id", type=sale_invoice_id, required=True)
+account_parser.add_argument(
+    "sale_invoice_id", type=sale_invoice_id_valid, required=True,
+)
 
 period_parser = reqparse.RequestParser()
 period_parser.add_argument("date_from", type=date, required=True)
