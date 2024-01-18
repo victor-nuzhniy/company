df91f2d94a8302ece54804318cb98abc802f0b4f
victro-nuzhniy <nuzhniyva@gmail.com>
2024-01-18 16:33:46 +0200

    formatters changed GetLastNameModelType location

--- api/apps/common_services/services.py
+++ api/apps/common_services/services.py
@@ -1,10 +1,24 @@
 """Services for common_services apps."""
 import inspect
+import typing
 
 from flask import abort
 from sqlalchemy import Row
 
-from api.typing import GetLastNameModelType
+from api.apps.invoice import models as invoice_models
+from api.apps.order import models as order_models
+from api.apps.purchase import models as purchase_models
+from api.apps.sale import models as sale_models
+from api.apps.tax import models as tax_models
+
+GetLastNameModelType = typing.Union[
+    order_models.Order,
+    invoice_models.Invoice,
+    purchase_models.PurchaseInvoice,
+    sale_models.SaleInvoice,
+    tax_models.TaxInvoice,
+    None,
+]
 
 
 def get_last_name(model_name: str) -> Row:
