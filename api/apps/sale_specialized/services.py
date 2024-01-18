f8e429c80c75b25b34025959a7116443abb05c66
victro-nuzhniy <nuzhniyva@gmail.com>
2024-01-18 16:03:31 +0200

    formatters changed sale_specialized apps models imports

--- api/apps/sale_specialized/services.py
+++ api/apps/sale_specialized/services.py
@@ -4,15 +4,11 @@ from typing import Sequence
 
 from sqlalchemy import and_, func, or_
 
-from api import (
-    Agreement,
-    Counterparty,
-    Invoice,
-    Product,
-    SaleInvoice,
-    SaleInvoiceProduct,
-    TaxInvoiceProduct,
-)
+from api.apps.counterparty import models as counterparty_models
+from api.apps.invoice import models as invoice_models
+from api.apps.product import models as product_models
+from api.apps.sale import models as sale_models
+from api.apps.tax import models as tax_models
 from api.constants import EARLIEST_DATE, LATEST_DATE
 
 
@@ -25,35 +21,43 @@ def get_sale_invoice_data(
     """Get SaleInvoice registry list."""
     date_from = date_from if date_from else EARLIEST_DATE
     date_to = date_to if date_to else LATEST_DATE
-    query = SaleInvoice.query.with_entities(
-        SaleInvoice.id.label("id"),
-        SaleInvoice.created_at.label("created_at"),
-        SaleInvoice.name.label("name"),
-        SaleInvoice.invoice_id.label("invoice_id"),
-        SaleInvoice.done.label("done"),
-        Invoice.name.label("invoice"),
-        Agreement.id.label("agreement_id"),
-        Agreement.name.label("agreement"),
-        Counterparty.id.label("counterparty_id"),
-        Counterparty.name.label("counterparty"),
-        func.sum((SaleInvoiceProduct.quantity * SaleInvoiceProduct.price)).label(
+    query = sale_models.SaleInvoice.query.with_entities(
+        sale_models.SaleInvoice.id.label("id"),
+        sale_models.SaleInvoice.created_at.label("created_at"),
+        sale_models.SaleInvoice.name.label("name"),
+        sale_models.SaleInvoice.invoice_id.label("invoice_id"),
+        sale_models.SaleInvoice.done.label("done"),
+        invoice_models.Invoice.name.label("invoice"),
+        counterparty_models.Agreement.id.label("agreement_id"),
+        counterparty_models.Agreement.name.label("agreement"),
+        counterparty_models.Counterparty.id.label("counterparty_id"),
+        counterparty_models.Counterparty.name.label("counterparty"),
+        func.sum(
+            (
+                sale_models.SaleInvoiceProduct.quantity
+                * sale_models.SaleInvoiceProduct.price
+            ),
+        ).label(
             "summ",
         ),
-        Product.currency.label("currency"),
+        product_models.Product.currency.label("currency"),
     )
     additional_query = (
-        query.outerjoin(SaleInvoice.sale_invoice_products)
-        .outerjoin(Invoice)
-        .outerjoin(Agreement)
-        .outerjoin(Counterparty)
-        .outerjoin(Product)
+        query.outerjoin(sale_models.SaleInvoice.sale_invoice_products)
+        .outerjoin(invoice_models.Invoice)
+        .outerjoin(counterparty_models.Agreement)
+        .outerjoin(counterparty_models.Counterparty)
+        .outerjoin(product_models.Product)
     )
     return (
         additional_query.filter(
-            and_(SaleInvoice.created_at > date_from, SaleInvoice.created_at < date_to),
+            and_(
+                sale_models.SaleInvoice.created_at > date_from,
+                sale_models.SaleInvoice.created_at < date_to,
+            ),
         )
-        .group_by(SaleInvoice)
-        .order_by(SaleInvoice.id.desc())
+        .group_by(sale_models.SaleInvoice)
+        .order_by(sale_models.SaleInvoice.id.desc())
         .limit(limit)
         .offset(offset)
         .all()
@@ -63,19 +67,19 @@ def get_sale_invoice_data(
 def get_sale_invoice_products_by_sale_invoice_id(sale_invoice_id: int) -> Sequence:
     """Get SaleInvoice products list by sale invoice id."""
     return (
-        SaleInvoiceProduct.query.with_entities(
-            SaleInvoiceProduct.id.label("id"),
-            SaleInvoiceProduct.product_id.label("product_id"),
-            SaleInvoiceProduct.quantity.label("quantity"),
-            SaleInvoiceProduct.price.label("price"),
-            SaleInvoiceProduct.sale_invoice_id.label("sale_invoice_id"),
-            Product.name.label("name"),
-            Product.code.label("code"),
-            Product.currency.label("currency"),
-            Product.units.label("units"),
+        sale_models.SaleInvoiceProduct.query.with_entities(
+            sale_models.SaleInvoiceProduct.id.label("id"),
+            sale_models.SaleInvoiceProduct.product_id.label("product_id"),
+            sale_models.SaleInvoiceProduct.quantity.label("quantity"),
+            sale_models.SaleInvoiceProduct.price.label("price"),
+            sale_models.SaleInvoiceProduct.sale_invoice_id.label("sale_invoice_id"),
+            product_models.Product.name.label("name"),
+            product_models.Product.code.label("code"),
+            product_models.Product.currency.label("currency"),
+            product_models.Product.units.label("units"),
         )
-        .join(Product)
-        .filter(SaleInvoiceProduct.sale_invoice_id == sale_invoice_id)
+        .join(product_models.Product)
+        .filter(sale_models.SaleInvoiceProduct.sale_invoice_id == sale_invoice_id)
         .all()
     )
 
@@ -91,53 +95,59 @@ def get_tax_sale_invoice_products_left(
     There are not products will be available from sale invoice with done True.
     """
     return (
-        SaleInvoiceProduct.query.with_entities(
-            SaleInvoiceProduct.id.label("id"),
-            SaleInvoiceProduct.product_id.label("product_id"),
-            SaleInvoiceProduct.quantity.label("quantity"),
-            SaleInvoiceProduct.price.label("price"),
-            SaleInvoiceProduct.sale_invoice_id.label("sale_invoice_id"),
+        sale_models.SaleInvoiceProduct.query.with_entities(
+            sale_models.SaleInvoiceProduct.id.label("id"),
+            sale_models.SaleInvoiceProduct.product_id.label("product_id"),
+            sale_models.SaleInvoiceProduct.quantity.label("quantity"),
+            sale_models.SaleInvoiceProduct.price.label("price"),
+            sale_models.SaleInvoiceProduct.sale_invoice_id.label("sale_invoice_id"),
             func.sum(
-                SaleInvoiceProduct.quantity
-                - TaxInvoiceProduct.query.with_entities(
-                    func.sum(TaxInvoiceProduct.quantity).label(
+                sale_models.SaleInvoiceProduct.quantity
+                - tax_models.TaxInvoiceProduct.query.with_entities(
+                    func.sum(tax_models.TaxInvoiceProduct.quantity).label(
                         "tax_invoice_product_sum",
                     ),
                 )
                 .filter(
-                    TaxInvoiceProduct.sale_invoice_product_id == SaleInvoiceProduct.id,
+                    tax_models.TaxInvoiceProduct.sale_invoice_product_id
+                    == sale_models.SaleInvoiceProduct.id,
                 )
                 .first()
                 .tax_invoice_product_sum,
             ).label("sale_products_left"),
-            Product.name.label("name"),
-            Product.code.label("code"),
-            Product.currency.label("currency"),
-            Product.units.label("units"),
+            product_models.Product.name.label("name"),
+            product_models.Product.code.label("code"),
+            product_models.Product.currency.label("currency"),
+            product_models.Product.units.label("units"),
         )
-        .join(Product)
-        .filter(SaleInvoiceProduct.sale_invoice_id == sale_invoice_id)
+        .join(product_models.Product)
+        .filter(sale_models.SaleInvoiceProduct.sale_invoice_id == sale_invoice_id)
         .filter(
             or_(
-                SaleInvoiceProduct.id.not_in(
-                    TaxInvoiceProduct.query.with_entities(
-                        TaxInvoiceProduct.sale_invoice_product_id,
-                    ).filter(TaxInvoiceProduct.tax_invoice_id == tax_invoice_id),
+                sale_models.SaleInvoiceProduct.id.not_in(
+                    tax_models.TaxInvoiceProduct.query.with_entities(
+                        tax_models.TaxInvoiceProduct.sale_invoice_product_id,
+                    ).filter(
+                        tax_models.TaxInvoiceProduct.tax_invoice_id == tax_invoice_id,
+                    ),
                 ),
                 and_(
-                    SaleInvoiceProduct.id.in_(
-                        TaxInvoiceProduct.query.with_entities(
-                            TaxInvoiceProduct.sale_invoice_product_id,
-                        ).filter(TaxInvoiceProduct.tax_invoice_id == tax_invoice_id),
+                    sale_models.SaleInvoiceProduct.id.in_(
+                        tax_models.TaxInvoiceProduct.query.with_entities(
+                            tax_models.TaxInvoiceProduct.sale_invoice_product_id,
+                        ).filter(
+                            tax_models.TaxInvoiceProduct.tax_invoice_id
+                            == tax_invoice_id,
+                        ),
                     ),
-                    SaleInvoiceProduct.quantity
-                    > TaxInvoiceProduct.query.with_entities(
-                        func.sum(TaxInvoiceProduct.quantity).label(
+                    sale_models.SaleInvoiceProduct.quantity
+                    > tax_models.TaxInvoiceProduct.query.with_entities(
+                        func.sum(tax_models.TaxInvoiceProduct.quantity).label(
                             "tax_invoice_product_sum",
                         ),
                     ).filter(
-                        TaxInvoiceProduct.sale_invoice_product_id
-                        == SaleInvoiceProduct.id,
+                        tax_models.TaxInvoiceProduct.sale_invoice_product_id
+                        == sale_models.SaleInvoiceProduct.id,
                     ),
                 ),
             ),
@@ -149,7 +159,7 @@ def get_tax_sale_invoice_products_left(
 def get_sale_invoices_by_agreement_id(agreement_id: int) -> Sequence:
     """Get SaleInvoice list by agreement_id."""
     return (
-        SaleInvoice.query.outerjoin(Invoice)
-        .filter(Invoice.agreement_id == agreement_id)
+        sale_models.SaleInvoice.query.outerjoin(invoice_models.Invoice)
+        .filter(invoice_models.Invoice.agreement_id == agreement_id)
         .all()
     )
