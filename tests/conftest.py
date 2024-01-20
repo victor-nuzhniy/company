f38155e1930f5f723410da8e565b939d1fef9fd7
victro-nuzhniy <nuzhniyva@gmail.com>
2024-01-20 23:22:54 +0200

    formatters refactor in connection with code changes in tests.conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,65 +5,31 @@ import typing
 
 import jwt
 import pytest
-from faker import Faker
 from flask import Flask
 from flask_restful import Api
 
 from api import User, db
-from api.apps.account.routes import (
-    IncomeForPeriodRoute,
-    PeriodReportRoute,
-    ProcessSaleInvoiceRoute,
-    ProductsLeftoversRoute,
-)
-from api.apps.auth.routes import AdminRoute, LoginRoute
-from api.apps.auth.utils import encrypt_password
-from api.apps.counterparty.routes import (
-    AgreementRoute,
-    AgreementsRoute,
-    CounterpartyRoute,
-    CounterpartysRoute,
-    DiscountRoute,
-    DiscountsRoute,
-)
-from api.apps.invoice.routes import (
-    InvoiceProductRoute,
-    InvoiceProductsRoute,
-    InvoiceRoute,
-    InvoicesRoute,
-)
-from api.apps.order.routes import (
-    OrderProductRoute,
-    OrderProductsRoute,
-    OrderRoute,
-    OrdersRoute,
-)
-from api.apps.product.routes import (
-    ProductRoute,
-    ProductsRoute,
-    ProductTypeRoute,
-    ProductTypesRoute,
-)
-from api.apps.purchase.routes import (
-    PurchaseInvoiceProductRoute,
-    PurchaseInvoiceProductsRoute,
-    PurchaseInvoiceRoute,
-    PurchaseInvoicesRoute,
-)
-from api.apps.sale.routes import (
-    SaleInvoiceProductRoute,
-    SaleInvoiceProductsRoute,
-    SaleInvoiceRoute,
-    SaleInvoicesRoute,
-)
-from api.apps.tax.routes import (
-    TaxInvoiceProductRoute,
-    TaxInvoiceProductsRoute,
-    TaxInvoiceRoute,
-    TaxInvoicesRoute,
-)
-from api.apps.user.routes import AdminUserRoute, UserRoute, UsersRoute
-from api.services import crud
+from api.apps.common.account import routes as account_routes
+from api.apps.common.auth import routes as auth_routes
+from api.apps.common.auth.auth_utilities import encrypt_password
+from api.apps.common.special import routes as common_special_routes
+from api.apps.counterparty.base import routes as counterparty_base_routes
+from api.apps.counterparty.special import routes as counterparty_special_routes
+from api.apps.invoice.base import routes as invoice_base_routes
+from api.apps.invoice.special import routes as invoice_special_routes
+from api.apps.order.base import routes as order_base_routes
+from api.apps.order.special import routes as order_special_routes
+from api.apps.product.base import routes as product_base_routes
+from api.apps.purchase.base import routes as purchase_base_routes
+from api.apps.purchase.special import routes as purchase_special_routes
+from api.apps.sale.base import routes as sale_base_routes
+from api.apps.sale.special import routes as sale_special_routes
+from api.apps.tax.base import routes as tax_base_routes
+from api.apps.tax.special import routes as tax_special_routes
+from api.apps.user.base import routes as user_base_routes
+from api.apps.user.special import routes as user_special_routes
+from api.common.api_types import ModelType
+from api.common.services import crud
 from tests.apps.counterparty.factories import (
     AgreementFactory,
     CounterpartyFactory,
@@ -88,95 +54,216 @@ def faker_seed() -> None:
     return random.seed(version=3)
 
 
-@pytest.fixture(autouse=True)
-def app():
+@pytest.fixture(scope="session", autouse=True)
+def app() -> Flask:  # noqa WPS213
     """Override dependencies for Flask and return flask instance."""
-    app = Flask(__name__)
-    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
-    app.config["TESTING"] = True
-    app.config["DEBUG"] = False
-    app.testing = True
-    api = Api(app)
-    api.add_resource(ProcessSaleInvoiceRoute, "/account/process-sale-invoice/")
-    api.add_resource(PeriodReportRoute, "/account/sale-report/")
-    api.add_resource(ProductsLeftoversRoute, "/account/product-leftovers/")
-    api.add_resource(IncomeForPeriodRoute, "/account/income-for-period/")
-    api.add_resource(LoginRoute, "/auth/login/")
-    api.add_resource(AdminRoute, "/auth/create-admin/")
+    application = Flask(__name__)
+    application.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+    application.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+    application.config["TESTING"] = True
+    application.config["DEBUG"] = False
+    application.testing = True
+    api = Api(application)
 
-    api.add_resource(DiscountsRoute, "/discount/")
-    api.add_resource(DiscountRoute, "/discount/<instance_id>")
-    api.add_resource(CounterpartyRoute, "/counterpary/,<instance_id>")
-    api.add_resource(CounterpartysRoute, "/counterparty/")
-    api.add_resource(AgreementRoute, "/agreement/<instance_id>")
-    api.add_resource(AgreementsRoute, "/agreement/")
-    api.add_resource(InvoiceRoute, "/invoice/<instance_id>")
-    api.add_resource(InvoicesRoute, "/invoice/")
-    api.add_resource(InvoiceProductRoute, "/invoice-product/<instance_id>")
-    api.add_resource(InvoiceProductsRoute, "/invoice-product/")
-    api.add_resource(OrderRoute, "/order/<instance_id>")
-    api.add_resource(OrdersRoute, "/order/")
-    api.add_resource(OrderProductRoute, "/order-product/<instance_id>")
-    api.add_resource(OrderProductsRoute, "/order-product/")
-    api.add_resource(ProductRoute, "/product/<instance_id>")
-    api.add_resource(ProductsRoute, "/product/")
-    api.add_resource(ProductTypeRoute, "/product-type/<instance_id>")
-    api.add_resource(ProductTypesRoute, "/product-type/")
-    api.add_resource(PurchaseInvoiceRoute, "/purchase-invoice/<instance_id>")
-    api.add_resource(PurchaseInvoicesRoute, "/purchase-invoice/")
     api.add_resource(
-        PurchaseInvoiceProductRoute, "/purchase-invoice-product/<instance_id>"
+        account_routes.ProcessSaleInvoiceRoute,
+        "/account/process-base-base/",
     )
-    api.add_resource(PurchaseInvoiceProductsRoute, "/purchase-invoice-product/")
-    api.add_resource(SaleInvoiceRoute, "/sale-invoice/<instance_id>")
-    api.add_resource(SaleInvoicesRoute, "/sale-invoice/")
-    api.add_resource(SaleInvoiceProductRoute, "/sale-invoice-product/<instance_id>")
-    api.add_resource(SaleInvoiceProductsRoute, "/sale-invoice-product/")
-    api.add_resource(TaxInvoiceRoute, "/tax-invoice/<instance_id>")
-    api.add_resource(TaxInvoicesRoute, "/tax-invoice/")
-    api.add_resource(TaxInvoiceProductRoute, "/tax-invoice-product/<instance_id>")
-    api.add_resource(TaxInvoiceProductsRoute, "/tax-invoice-product/")
-    api.add_resource(UserRoute, "/user/<instance_id>")
-    api.add_resource(UsersRoute, "/user/")
-    api.add_resource(AdminUserRoute, "/user/admin/<user_id>")
+    api.add_resource(account_routes.PeriodReportRoute, "/account/base-report/")
+    api.add_resource(account_routes.ProductsLeftoversRoute, "/account/base-leftovers/")
+    api.add_resource(account_routes.IncomeForPeriodRoute, "/account/income-for-period/")
 
-    db.init_app(app)
-    with app.app_context():
+    api.add_resource(auth_routes.LoginRoute, "/auth/login/")
+    api.add_resource(auth_routes.AdminRoute, "/auth/create-admin/")
+
+    api.add_resource(common_special_routes.NameNumberRoute, "/account/<model_name>/")
+
+    api.add_resource(counterparty_base_routes.DiscountRoute, "/discount/<instance_id>/")
+    api.add_resource(counterparty_base_routes.DiscountsRoute, "/discount/")
+    api.add_resource(
+        counterparty_base_routes.CounterpartyRoute,
+        "/counterpary/,<instance_id>/",
+    )
+    api.add_resource(counterparty_base_routes.CounterpartysRoute, "/counterparty/")
+    api.add_resource(
+        counterparty_base_routes.AgreementRoute,
+        "/agreement/<instance_id>/",
+    )
+    api.add_resource(counterparty_base_routes.AgreementsRoute, "/agreement/")
+
+    api.add_resource(
+        counterparty_special_routes.CounterpartyAgreementsRoute,
+        "/agreements/<company_id>/",
+    )
+
+    api.add_resource(invoice_base_routes.InvoiceRoute, "/invoice/<instance_id>/")
+    api.add_resource(invoice_base_routes.InvoicesRoute, "/invoice/")
+    api.add_resource(
+        invoice_base_routes.InvoiceProductRoute,
+        "/invoice-product/<instance_id>/",
+    )
+    api.add_resource(invoice_base_routes.InvoiceProductsRoute, "/invoice-product/")
+
+    api.add_resource(invoice_special_routes.InvoiceRegistryRoute, "/invoice-registry/")
+    api.add_resource(
+        invoice_special_routes.InvoicesProductsRoute,
+        "/invoice-products/<invoice_id>/",
+    )
+    api.add_resource(
+        invoice_special_routes.AgreementInvoicesRoute,
+        "/invoices/<agreement_id>/",
+    )
+
+    api.add_resource(order_base_routes.OrderRoute, "/order/<instance_id>/")
+    api.add_resource(order_base_routes.OrdersRoute, "/order/")
+    api.add_resource(
+        order_base_routes.OrderProductRoute,
+        "/order-product/<instance_id>/",
+    )
+    api.add_resource(order_base_routes.OrderProductsRoute, "/order-product/")
+
+    api.add_resource(order_special_routes.OrderRegistryRoute, "/order-registry/")
+    api.add_resource(
+        order_special_routes.OrdersProductsRoute,
+        "/orders-products/<order_id>/",
+    )
+    api.add_resource(order_special_routes.UserOrderRoute, "/user-order/")
+    api.add_resource(
+        order_special_routes.CounterpartyOrdersRoute,
+        "/orders/<company_id>/",
+    )
+
+    api.add_resource(product_base_routes.ProductRoute, "/product/<instance_id>/")
+    api.add_resource(product_base_routes.ProductsRoute, "/product/")
+    api.add_resource(
+        product_base_routes.ProductTypeRoute,
+        "/product-type/<instance_id>/",
+    )
+    api.add_resource(product_base_routes.ProductTypesRoute, "/product-type/")
+
+    api.add_resource(
+        purchase_base_routes.PurchaseInvoiceRoute,
+        "/purchase-invoice/<instance_id>/",
+    )
+    api.add_resource(purchase_base_routes.PurchaseInvoicesRoute, "/purchase-invoice/")
+    api.add_resource(
+        purchase_base_routes.PurchaseInvoiceProductRoute,
+        "/purchase-invoice-product/<instance_id>/",
+    )
+    api.add_resource(
+        purchase_base_routes.PurchaseInvoiceProductsRoute,
+        "/purchase-invoice-product/",
+    )
+
+    api.add_resource(
+        purchase_special_routes.PurchaseRegistryRoute,
+        "/purchase-registry/",
+    )
+    api.add_resource(
+        purchase_special_routes.PurchaseInvoicesProductsRoute,
+        "/purchase-invoice-products/<purchase_invoice_id>/",
+    )
+    api.add_resource(
+        purchase_special_routes.PurchaseInvoiceProductsLeftRoute,
+        "/purchase-invoice-products/product/<product_id>/",
+    )
+
+    api.add_resource(sale_base_routes.SaleInvoiceRoute, "/sale-invoice/<instance_id>/")
+    api.add_resource(sale_base_routes.SaleInvoicesRoute, "/sale-invoice/")
+    api.add_resource(
+        sale_base_routes.SaleInvoiceProductRoute,
+        "/sale-invoice-product/<instance_id>/",
+    )
+    api.add_resource(
+        sale_base_routes.SaleInvoiceProductsRoute,
+        "/sale-invoice-product/",
+    )
+
+    api.add_resource(sale_special_routes.SaleRegistryRoute, "/sale-invoice-registry/")
+    api.add_resource(
+        sale_special_routes.SaleInvoicesProductsRoute,
+        "/sale-invoice-products/<sale_invoice_id>/",
+    )
+    api.add_resource(
+        sale_special_routes.TaxSaleInvoiceProductsLeftRoute,
+        "/sale-invoice-products/<sale_invoice_id>/<tax_invoice_id>/",
+    )
+    api.add_resource(
+        sale_special_routes.AgreementSaleInvoicesRoute,
+        "/agreement-sale-invoices/<agreement_id>/",
+    )
+
+    api.add_resource(tax_base_routes.TaxInvoiceRoute, "/tax-invoice/<instance_id>/")
+    api.add_resource(tax_base_routes.TaxInvoicesRoute, "/tax-invoice/")
+    api.add_resource(
+        tax_base_routes.TaxInvoiceProductRoute,
+        "/tax-invoice-product/<instance_id>/",
+    )
+    api.add_resource(tax_base_routes.TaxInvoiceProductsRoute, "/tax-invoice-product/")
+
+    api.add_resource(tax_special_routes.TaxRegistryRoute, "/tax-registry/")
+    api.add_resource(
+        tax_special_routes.TaxInvoicesProductsRoute,
+        "/tax-invoice-products/<tax_invoice_id>/",
+    )
+    api.add_resource(
+        tax_special_routes.TaxInvoiceProductCreateRoute,
+        "/tax-invoice-product-create/",
+    )
+    api.add_resource(
+        tax_special_routes.TaxInvoiceProductDeleteRoute,
+        "/tax-invoice-product-delete/<tax_invoice_product_id>/",
+    )
+    api.add_resource(
+        tax_special_routes.TaxInvoiceDeleteRoute,
+        "/tax-invoice-delete/<tax_invoice_id>/",
+    )
+
+    api.add_resource(user_base_routes.UserRoute, "/user/<instance_id>/")
+    api.add_resource(user_base_routes.UsersRoute, "/user/")
+
+    api.add_resource(user_special_routes.AdminUserRoute, "/user/admin/<user_id>/")
+
+    db.init_app(application)
+    with application.app_context():
         db.create_all()
-        yield app
+        yield application
         db.drop_all()
 
 
-@pytest.fixture
-def admin_user(faker: Faker) -> User:
-    """Create admin user."""
-    admin = crud.create(
+@pytest.fixture(scope="session")
+def admin_user() -> User:
+    """Create admin base."""
+    return crud.create(
         User,
         {
-            "username": faker.user_name(),
-            "email": faker.email(),
+            "username": "administrator",
+            "email": "elegance@abc.com",
             "password": encrypt_password("11111"),
             "is_admin": True,
             "is_active": True,
         },
     )
-    return admin
 
 
-@pytest.fixture
-def auth_header(admin_user: User, app: Flask) -> typing.Dict:
-    """Get admin user authorization token."""
+@pytest.fixture(scope="session")
+def auth_header(admin: User, application: Flask) -> dict:
+    """Get admin base authorization token."""
     token = jwt.encode(
-        {"user_id": admin_user.id}, app.config["SECRET_KEY"], algorithm="HS256"
+        {"user_id": admin.id},
+        application.config["SECRET_KEY"],
+        algorithm="HS256",
     )
-    return {"Authorization": f"Bearer {token}", "Content-Type": "application/json"}
+    return {
+        "Authorization": "Bearer {token}".format(token=token),
+        "Content-Type": "application/json",
+    }
 
 
 @pytest.fixture(autouse=True, scope="session")
 def set_session_for_factories() -> None:
     """Register model factories to set up a scoped session during the test run."""
-    known_factories: typing.List[typing.Type[BaseModelFactory]] = [
+    known_factories: list[typing.Type[BaseModelFactory]] = [
         AgreementFactory,
         CounterpartyFactory,
         DiscountFactory,
@@ -197,36 +284,35 @@ def set_session_for_factories() -> None:
 
     for factory_class in known_factories:
         # Set up session to factory
-        factory_class._meta.sqlalchemy_session = db.session
+        factory_class._meta.sqlalchemy_session = db.session  # noqa WPS347
 
 
 def check_instance_expected_data(
-    response: typing.Any, expected_data: typing.Dict | db.Model
+    response: typing.Any,
+    expected_data: dict | ModelType,
 ) -> None:
     """
     Check response status whether it's 200.
 
     Compare instance data with expected.
     """
-    result = response.get_json()
+    result_response = response.get_json()
     assert response.status_code == 200
     if isinstance(expected_data, typing.Dict):
-        for key, value in expected_data.items():
-            if key in result:
-                assert result[key] == value
+        for key, elem in expected_data.items():
+            if key in result_response:
+                assert result_response[key] == elem
     else:
-        for key, value in result.items():
-            if key == "created_at":
-                assert (
-                    getattr(expected_data, key).strftime("%Y-%m-%dT%H:%M:%S") == value
-                )
+        for ky, el in result_response.items():
+            if ky == "created_at":
+                assert getattr(expected_data, ky).strftime("%Y-%m-%dT%H:%M:%S") == el
             else:
-                assert getattr(expected_data, key) == value
+                assert getattr(expected_data, ky) == el
 
 
-def delete_random_dict_key(data: typing.Dict) -> typing.Dict:
+def delete_random_dict_key(data_dict: dict) -> dict:
     """Delete random dict key."""
-    if len(data) > 1:
-        key = random.choice(list(data.keys()))
-        data.pop(key)
-    return data
+    if len(data_dict) > 1:
+        key = random.choice(list(data_dict.keys()))
+        data_dict.pop(key)
+    return data_dict
