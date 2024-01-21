783b1246f5cb0e181a879ad283772143e2ee2802
victro-nuzhniy <nuzhniyva@gmail.com>
2024-01-21 23:03:46 +0200

    formatters created tests order base

---
+++ tests/apps/order/base/test_routes.py
@@ -0,0 +1,43 @@
+"""Module for testing base routes."""
+from typing import Dict
+
+from faker import Faker
+
+from api import Order, OrderProduct
+from tests.apps.order.base.testing_utilities import (
+    create_order_data,
+    create_order_product_data,
+    create_order_put_data,
+)
+from tests.apps.order.base.factories import OrderFactory, OrderProductFactory
+from tests.testing_classes import SampleTestRoute
+
+
+class TestOrderRoute(SampleTestRoute):
+    """Class with methods for testing Order routes."""
+
+    model = Order
+    factory = OrderFactory
+
+    def get_fake_data(self, faker: Faker) -> Dict:
+        """Get Order fake data dict."""
+        return create_order_data(faker)
+
+    def get_fake_put_data(self, faker: Faker) -> Dict:
+        """Get Oder fake data dict for put and patch methods."""
+        return create_order_put_data(faker)
+
+
+class TestOrderProductRoute(SampleTestRoute):
+    """Class with methods for testing OrderProduct routes."""
+
+    model = OrderProduct
+    factory = OrderProductFactory
+
+    def get_fake_data(self, faker: Faker) -> Dict:
+        """Get OrderProduct fake data dict."""
+        return create_order_product_data(faker)
+
+    def get_fake_put_data(self, faker: Faker) -> Dict:
+        """Get OderProduct fake data dict for put and patch methods."""
+        return create_order_product_data(faker)
---
+++ tests/apps/order/base/testing_utilities.py
@@ -0,0 +1,45 @@
+"""Fixtures for order apps."""
+from typing import Dict
+
+from faker import Faker
+
+from api import Counterparty, Order, Product, User
+from tests.apps.counterparty.base.factories import CounterpartyFactory
+from tests.apps.order.base.factories import OrderFactory
+from tests.apps.product.base.factories import ProductFactory
+from tests.apps.user.base.factories import UserFactory
+
+
+def create_order_data(faker: Faker) -> Dict:
+    """Create Order fake data."""
+    user: User = UserFactory()
+    counterparty: Counterparty = CounterpartyFactory()
+    return {
+        "user_id": user.id,
+        "name": faker.user_name(),
+        "customer_id": counterparty.id,
+    }
+
+
+def create_order_put_data(faker: Faker) -> Dict:
+    """Create Order fake data for put and patch methods.."""
+    user: User = UserFactory()
+    counterparty: Counterparty = CounterpartyFactory()
+    return {
+        "user_id": user.id,
+        "name": faker.user_name(),
+        "created_at": faker.date_time().strftime("%Y-%m-%dT%H:%M:%S"),
+        "customer_id": counterparty.id,
+    }
+
+
+def create_order_product_data(faker: Faker) -> Dict:
+    """Create OrderProduct fake data."""
+    product: Product = ProductFactory()
+    order: Order = OrderFactory()
+    return {
+        "product_id": product.id,
+        "quantity": faker.pyint(),
+        "price": faker.pyint(),
+        "order_id": order.id,
+    }
