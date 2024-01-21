35e8f7b6e37172fccf3235b839f1e087a39ce32f
victro-nuzhniy <nuzhniyva@gmail.com>
2024-01-21 11:51:04 +0200

    formatters refactor in connection with code changes in tests.conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -55,7 +55,7 @@ def faker_seed() -> None:
 
 
 @pytest.fixture(scope="session", autouse=True)
-def app() -> Flask:  # noqa WPS213
+def app() -> _FixtureFunc[Flask]:  # noqa WPS213
     """Override dependencies for Flask and return flask instance."""
     application = Flask(__name__)
     application.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
